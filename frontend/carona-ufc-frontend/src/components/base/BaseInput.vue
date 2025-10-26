1e8c5c4d4ce619bb000c882cd43ec325cb7499c9
Rian Lima <rlimamendes@proton.me>
2025-10-25 23:46:19 -0300

    Componentes base para inputs e botões

---
+++ frontend/carona-ufc-frontend/src/components/base/BaseInput.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+
+const model = defineModel<string | number>()
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  required: {
+    type: Boolean,
+    default: false
+  },
+})
+
+</script>
+
+<template>
+  <div class="w-full">
+    <label
+      :for="id"
+      class="block text-sm font-medium text-gray-700"
+    >
+      {{ label }}
+    </label>
+
+    <input
+      v-model="model"
+      :type="type"
+      :id="id"
+      :name="id"
+      :placeholder="placeholder"
+      :required="required"
+      class="
+        mt-1 block w-full rounded-md border-gray-300 px-3 py-2
+        shadow-sm transition-colors duration-200
+        focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500
+      "
+    >
+  </div>
+</template>
--- frontend/carona-ufc-frontend/tailwind.config.js
+++ frontend/carona-ufc-frontend/tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // 1. Diga ao Tailwind onde procurar por classes
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    // 2. (Opcional) Podemos estender o tema aqui
     extend: {
       colors: {
-        'ufc-green': '#005a3b', // Cor da UFC para usarmos
+        'ufc-green': '#005a3b',
       },
       fontFamily: {
-        // 3. Garante que a fonte 'Inter' seja a padrão
         sans: ['Inter', 'sans-serif'],
       }
     },
