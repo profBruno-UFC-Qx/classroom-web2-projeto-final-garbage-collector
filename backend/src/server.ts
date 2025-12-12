0101b68ff4cbe86b46edbc6dd63a431f8f0216a9
Rian Lima <rlimamendes@proton.me>
2025-12-11 21:30:41 -0300

    Configuração da conexão com banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,16 @@
-import app from './app';
+import "reflect-metadata";
+import app from "./app";
+import { AppDataSource } from "./config/data-source";
 
 const PORT = process.env.PORT || 3000;
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Banco de dados inicializado com sucesso.");
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Erro na inicialização do banco de dados:", err);
+  });
